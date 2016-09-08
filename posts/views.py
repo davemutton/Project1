8076a8f0cebd4cfb97390c0c38690b19fba1e994
sulav <sulav67@gmail.com>
2016-09-07 20:25:17 -0500

    Added form for the post

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .forms import PostForm, CategoryForm
 from .models import Post, Category
-
-
 
 
 def post_list(request):
@@ -11,18 +9,25 @@ def post_list(request):
     return render(request, 'posts/post_list.html', {'post_list': queryset})
 
 def post_detail(request, year, month, day, post):
-    post = Post.objects.get(slug            = post,
-                            status          = 'published',
-                            publish__year    = year,
-                            publish__month   = month,
-                            publish__day     = day)
-    return render(request, 'posts/post_detail.html', {'post':post})
+    instance = get_object_or_404(Post, slug            = post,
+                                       status          = 'published',
+                                       publish__year   = year,
+                                       publish__month  = month,
+                                       publish__day    = day)
+    return render(request, 'posts/post_detail.html', {'post':instance})
 
 # - I would change line 124 to this: post = Post.objects.last(), just to make sure you can get any value
 #[18:27] <mcspud> And then once that works, experiment with .get()
 
 def post_create(request):
-    return HttpResponse('post create')
+    post_form = PostForm()
+    category_form = CategoryForm()
+    
+    if request.method == 'POST':
+        print (request.POST)
+
+    context = {'post_form':post_form, 'category_form':category_form}
+    return render(request, 'posts/post_form.html', context)
 
 def post_update(request):
     return HttpResponse('post update')
